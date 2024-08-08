506b72967d6413c5f9057038c77be11485e2c8d8
Arinjay Pathak <61341752+Arinjay11020@users.noreply.github.com>
2024-08-08 19:56:06 +0530

    Update finetune_lora.sh

--- coop_new/LLM-Adapters/finetune_lora.sh
+++ coop_new/LLM-Adapters/finetune_lora.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=3 python finetune.py \
   --base_model '/home/models/Llama-2-7b-hf' \
   --data_path 'ft-training_set/math_10k.json' \
-  --output_dir './final_trained_models/cs/lora_A' \
+  --output_dir './final_trained_models/llama2/math/lora_A' \
   --batch_size 4 \
   --micro_batch_size 4 \
   --num_epochs 5 \
