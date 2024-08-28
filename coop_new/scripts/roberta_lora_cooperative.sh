17a24f3a664230a1216a0bb91e34fc3e2219a009
Arinjay Pathak <61341752+Arinjay11020@users.noreply.github.com>
2024-08-28 12:09:17 +0530

    Update roberta_lora_cooperative.sh

--- coop_new/scripts/roberta_lora_cooperative.sh
+++ coop_new/scripts/roberta_lora_cooperative.sh
@@ -39,7 +39,7 @@ TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=1 WANDB_PROJECT='Roberta-Cooperativ
 --apply_lora \
 --lora_r 8 \
 --lora_alpha 16 \
---post 0 \
+--posthoc_app 0 \
 --output_dir ../outputs/roberta/${task}/cooperative_lora/posthoc/${num_epochs}/${num_coop_epochs}/${vls}/${lr}/${lora_r}/${lora_alpha}/${num_experts} \
 --overwrite_output_dir
 done
