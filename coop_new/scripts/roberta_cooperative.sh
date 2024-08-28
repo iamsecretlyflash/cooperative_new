6eb73cfec5f942b86b5e25a466c0fff8bb21d37e
Arinjay Pathak <61341752+Arinjay11020@users.noreply.github.com>
2024-08-28 12:19:27 +0530

    Update roberta_cooperative.sh

--- coop_new/scripts/roberta_cooperative.sh
+++ coop_new/scripts/roberta_cooperative.sh
@@ -1,12 +1,15 @@
-tasks=('mrpc')
-eval_st=(115)
-batch=(32)
+tasks=('mrpc' 'cola' 'rte' 'wic' 'boolq')
+eval_st=(115 268 156 170 295)
+batch=(32 32 16 32 32)
 num_experts=4
-learn_rate=(5e-5)
+learn_rate=(5e-5 2e-5 3e-5 3e-5 3e-5)
 sample_period=1
 max_seq_length=128
 vls=5e-3
 kl_loss_weight=1e-5
+num_epochs=10
+num_coop_epochs=10
+num_std_epochs=0
 for i in "${!tasks[@]}";
 do
 task="${tasks[$i]}"
@@ -24,14 +27,15 @@ TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=0 WANDB_PROJECT='Roberta-Cooperativ
 --task_name ${task} \
 --do_train --do_eval --max_seq_length ${max_seq_length} \
 --per_device_train_batch_size ${bs} --learning_rate ${lr} \
---num_train_epochs 10 \
---num_coop_epochs 10 \
---num_std_epochs 0 \
+--num_train_epochs ${num_epochs} \
+--num_coop_epochs ${num_coop_epochs} \
+--num_std_epochs ${num_std_epochs} \
 --evaluation_strategy steps --eval_steps ${eval_steps} \
 --save_strategy steps --save_steps 5000 \
 --logging_steps 10 \
+--posthoc_app 0 \
 --report_to wandb \
 --seed 6  \
---output_dir ../outputs/roberta/${task}/query/${lr}/check/inverse/b15 \
+--output_dir ../outputs/roberta/${task}/query/posthoc/${num_epochs}/${num_coop_epochs}/${vls}/${lr}/${num_experts} \
 --overwrite_output_dir
 done
