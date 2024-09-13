e9b1bcc28912ff10112e34611897285f478f0ce9
Arinjay11020 <arinjay11020@gmail.com>
2024-09-13 17:53:16 +0530

    Added script t5_cooperative.sh

---
+++ coop_new/scripts/t5_cooperative.sh
@@ -0,0 +1,45 @@
+tasks=('mrpc' 'cola' 'rte' 'stsb') #'sst2' 'qnli' 'mnli' 'qqp')
+eval_st=(115 268 156 180 2105) #3274 12272 11371)
+batch=(32 32 16 32) #32 32 32 32)
+num_experts=8
+learn_rate=(3e-5 3e-5 3e-5 3e-5) #3e-5 3e-5 3e-5 3e-5)
+sample_period=1
+max_seq_length=128
+vls=5e-3
+kl_loss_weight=1e-5
+num_epochs=20
+num_coop_epochs=20
+num_std_epochs=0
+use_entropy=False
+for use_entropy in False True;
+do
+for i in "${!tasks[@]}";
+do
+task="${tasks[$i]}"
+eval_steps="${eval_st[$i]}"
+bs="${batch[$i]}"
+lr="${learn_rate[$i]}"
+TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=0 WANDB_PROJECT='T5-Cooperative-PostHoc' CUDA_VISIBLE_DEVICES=2 python /home/arinjay/fix/cooperative_new/coop_new/examples/text-classification/run_glue.py \
+--model_name_or_path t5-base \
+--expert_locations 'query' \
+--num_experts ${num_experts} \
+--var_loss_scale ${vls} \
+--use_entropy ${use_entropy} \
+--sample_period ${sample_period} \
+--kl_loss_weight ${kl_loss_weight} \
+--task_name ${task} \
+--do_train --do_eval --max_seq_length ${max_seq_length} \
+--per_device_train_batch_size ${bs} --learning_rate ${lr} \
+--num_train_epochs ${num_epochs} \
+--num_coop_epochs ${num_coop_epochs} \
+--num_std_epochs ${num_std_epochs} \
+--posthoc_app 0 \
+--evaluation_strategy steps --eval_steps ${eval_steps} \
+--save_strategy steps --save_steps 5000 \
+--logging_steps 10 \
+--report_to wandb \
+--seed 6  \
+--output_dir ../outputs/T5/${task}/query/latest_coop/${num_epochs}/${num_coop_epochs}/${vls}/${lr}/${num_experts}/${use_entropy} \
+--overwrite_output_dir
+done
+done
