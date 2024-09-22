f71da9bfcf4dc2e3d03d98b0cb9bd224f4ab5a90
Vaibhav Seth <96828194+iamsecretlyflash@users.noreply.github.com>
2024-09-22 21:19:49 +0530

    Create deberta_lora_cooperative_qkv.sh
    All the configurations are run for smaller tasks first and then switches over to larger tasks

---
+++ coop_new/scripts/deberta_lora_cooperative_qkv.sh
@@ -0,0 +1,103 @@
+tasks=('mrpc' 'cola' 'rte' 'stsb')
+eval_st=(115 268 78 180)
+num_experts=4
+lr=9e-4
+batch=(32 32 32 32)
+sample_period=1
+max_seq_length=128
+vls=5e-3
+kl_loss_weight=1e-5
+num_epochs=10
+num_coop_epochs=10
+num_std_epochs=0
+lora_r=8
+lora_alpha=16
+use_entropy=False
+for use_entropy in True;
+do
+for i in "${!tasks[@]}";
+do
+task="${tasks[$i]}"
+eval_steps="${eval_st[$i]}"
+bs="${batch[$i]}"
+TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=1 WANDB_PROJECT='Deberta-Cooperative-PostHoc' CUDA_VISIBLE_DEVICES=1 python ../examples/text-classification/run_glue.py  \
+--model_name_or_path microsoft/deberta-v3-base \
+--target_modules 'query_proj,key_proj,value_proj' \
+--lora_cooperative_at 'lora_A' \
+--cooperative_targets 'query_proj,key_proj,value_proj' \
+--num_experts ${num_experts} \
+--var_loss_scale ${vls} \
+--use_entropy ${use_entropy} \
+--sample_period ${sample_period} \
+--kl_loss_weight ${kl_loss_weight} \
+--task_name ${task} \
+--do_train --do_eval --max_seq_length ${max_seq_length} \
+--per_device_train_batch_size ${bs} --learning_rate ${lr} \
+--num_train_epochs ${num_epochs} \
+--num_coop_epochs ${num_coop_epochs} \
+--num_std_epochs ${num_std_epochs} \
+--evaluation_strategy steps --eval_steps ${eval_steps}  \
+--save_strategy steps --save_steps 100000000 \
+--logging_steps 10 \
+--report_to wandb \
+--seed 6  \
+--apply_lora \
+--lora_r ${lora_r} \
+--lora_alpha ${lora_alpha} \
+--posthoc_app 0 \
+--output_dir ../outputs/deberta/${task}/cooperative_lora/lora-qkv-coop-qkv/latest_coop/${num_epochs}/${num_coop_epochs}/${vls}/${lr}/${lora_r}/${lora_alpha}/${num_experts}/${use_entropy} \
+--overwrite_output_dir
+done
+done
+
+tasks=( 'sst2' 'qnli' 'mnli' 'qqp')
+eval_st=( 2105 3274 12272 11371)
+num_experts=4
+lr=9e-4
+batch=( 32 32 32 32)
+sample_period=1
+max_seq_length=128
+vls=5e-3
+kl_loss_weight=1e-5
+num_epochs=10
+num_coop_epochs=10
+num_std_epochs=0
+lora_r=8
+lora_alpha=16
+use_entropy=False
+for use_entropy in True;
+do
+for i in "${!tasks[@]}";
+do
+task="${tasks[$i]}"
+eval_steps="${eval_st[$i]}"
+bs="${batch[$i]}"
+TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=1 WANDB_PROJECT='Deberta-Cooperative-PostHoc' CUDA_VISIBLE_DEVICES=0 python ../examples/text-classification/run_glue.py  \
+--model_name_or_path microsoft/deberta-v3-base \
+--target_modules 'query_proj,key_proj,value_proj' \
+--lora_cooperative_at 'lora_A' \
+--cooperative_targets 'query_proj,key_proj,value_proj' \
+--num_experts ${num_experts} \
+--var_loss_scale ${vls} \
+--use_entropy ${use_entropy} \
+--sample_period ${sample_period} \
+--kl_loss_weight ${kl_loss_weight} \
+--task_name ${task} \
+--do_train --do_eval --max_seq_length ${max_seq_length} \
+--per_device_train_batch_size ${bs} --learning_rate ${lr} \
+--num_train_epochs ${num_epochs} \
+--num_coop_epochs ${num_coop_epochs} \
+--num_std_epochs ${num_std_epochs} \
+--evaluation_strategy steps --eval_steps ${eval_steps}  \
+--save_strategy steps --save_steps 100000000 \
+--logging_steps 10 \
+--report_to wandb \
+--seed 6  \
+--apply_lora \
+--lora_r ${lora_r} \
+--lora_alpha ${lora_alpha} \
+--posthoc_app 0 \
+--output_dir ../outputs/deberta/${task}/cooperative_lora/lora-qkv-coop-qkv/latest_coop/${num_epochs}/${num_coop_epochs}/${vls}/${lr}/${lora_r}/${lora_alpha}/${num_experts}/${use_entropy} \
+--overwrite_output_dir
+done
+done
